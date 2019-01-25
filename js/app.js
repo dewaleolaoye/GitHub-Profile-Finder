d093235d85fb227339f6f51a9840af3ab6bcb760
Engr - six <esix@localhost.localdomain>
2019-01-25 09:44:29 +0100

    other details

--- js/app.js
+++ js/app.js
@@ -19,9 +19,22 @@ $(document).ready(function(){
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-3">
-                            <img class="thumbnail" src="${user.avatar_url}"
-                        </div>
+                            <img class="thumbnail avatar" src="${user.avatar_url}">
+                            <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}"> View Profile </a>
+                            </div>
                         <div class="col-md-9">
+                            <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                            <span class="label label-primary">Public Gist: ${user.public_gists}</span>
+                            <span class="label label-success">Followers: ${user.followers}</span>
+                            <span class="label label-info">Following: ${user.following}</span>
+                            <br><br>
+
+                            <ul class="list-group">
+                                <li class ="list-group-item">Company: ${user.company}</li>
+                                <li class ="list-group-item">Website/Blog: ${user.blog}</li>
+                                <li class ="list-group-item">Location ${user.location}</li>
+                                <li class ="list-group-item">Member Since: ${user.created_at}</li>
+                            </ul>
 
                         </div>
                     </div>
@@ -31,3 +44,14 @@ $(document).ready(function(){
         });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
