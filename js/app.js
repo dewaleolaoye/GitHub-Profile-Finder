890aa8ae5b1f79662162f7908ec86cbc88f22539
Engr - six <esix@localhost.localdomain>
2019-01-25 07:47:10 +0100

    jQuery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-    console.log("ready")
+    $("#searchUser").on('keypress', function(e){
+        let username = e.target.value;
+
+        // Make request to Github
+
+        $.ajax({
+            url: 'https://api.github.com/users/' + username,
+            data: {
+                client_id: '120d3a664413424830f9',
+                client_secret: 'e17a653f7649ebc218fc98d29a4867b186578d5e'
+            }
+        }).done(function(user){
+            $('#profile').html(`
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">${user.name}</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3>
+                            <img src="${user.avatar_url}">
+                        <div>
+                        <div class="col-md-9>
+
+                        <div>
+                    <div>
+                </div>
+            </div>
+            `)
+        });
+    });
 });
